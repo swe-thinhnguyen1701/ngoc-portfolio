5b624acdfbca4d2bd19246fd6e3f287ea432bbf2
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-10-02 22:53:20 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import NavBar from "./components/NavBar"
+import Footer from "./components/Footer"
+import Main from "./components/Main"
+
+function App() {
+
+  return (
+    <>
+      <NavBar />
+      <Main />
+      <Footer />
+    </>
+  )
+}
+
+export default App
