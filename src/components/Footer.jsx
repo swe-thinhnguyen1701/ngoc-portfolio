f2abbb866f88989e252f930a8ad8d437aaecae9d
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-10-02 22:51:47 -0500

    build footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer () {
+    return (
+        <footer className="footer">
+            <p>Ngoc Trinh © 2024 - All rights reserved</p>
+        </footer>
+    )
+}
+
+export default Footer;
