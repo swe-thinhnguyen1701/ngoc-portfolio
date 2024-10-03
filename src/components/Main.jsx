5d132d3558753f323206024a5250b790a9c01ec1
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-10-02 22:52:35 -0500

    build main component

---
+++ src/components/Main.jsx
@@ -0,0 +1,15 @@
+import Greeting from "./Greeting";
+import About from "./About";
+import Education from "./Education";
+
+function Main () {
+    return (
+        <main>
+            <Greeting />
+            <About />
+            <Education />
+        </main>
+    )
+}
+
+export default Main;
