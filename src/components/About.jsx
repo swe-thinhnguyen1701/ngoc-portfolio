3c3fd762f1de20915d78bd1a122eb4e7ec049b74
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-10-02 22:52:05 -0500

    build about section

---
+++ src/components/About.jsx
@@ -0,0 +1,25 @@
+function About() {
+    return (
+        <section id="about">
+            <div className="about-title">
+                <span>About Me</span>
+            </div>
+            <div className="about-content">
+                <span>
+                    Hello! I’m Ngoc Trinh, a dedicated and passionate Nurse Practitioner currently pursuing my Direct Entry MSN at Marquette University. My journey in healthcare began with a profound interest in the human body and mind, which led me to earn my BS in Neurobiology, Physiology, and Behavior from the University of California, Davis. I also hold an AS in Biological Science from De Anza College.
+                </span>
+                <span>
+                    Over the past several years, I’ve gained diverse hands-on experience through roles such as a Phlebotomist at Octapharma Plasma Center and Quest Diagnostics, where I performed over 2000 venipunctures and assisted patients through various medical procedures. My experience extends to working as an ECG Technician, identifying abnormal heart rhythms, and as a Medical Receptionist, where I managed over 3000 patient appointments and insurance claims, honing both my clinical and administrative skills.
+                </span>
+                <span>
+                    My approach to patient care is rooted in empathy, clear communication, and a deep commitment to promoting health and wellness. In addition to my professional experience, I have volunteered at VN CARES Student Run Clinic, helping to promote cancer awareness in underserved communities and assisting patients facing language barriers.
+                </span>
+                <span>
+                    I’m excited to continue growing as a healthcare provider, combining my clinical expertise with a compassionate, patient-centered approach to make a meaningful difference in people’s lives.
+                </span>
+            </div>
+        </section>
+    )
+}
+
+export default About;
