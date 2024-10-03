bcf7277c8e8a3e0b941ec97825064cec5ee827eb
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-10-02 22:51:58 -0500

    build greeting component

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,12 @@
+function Greeting() {
+    return (
+        <section className="greeting">
+            <span className="name">Ngoc Trinh</span>
+            <span className="major">Registered Nurse</span>
+            <p>Providing compassionate care and expert medical advice for healthier lives.</p>
+            <a href="#contact" className="link contact-btn">Contact Me</a>
+        </section>
+    )
+}
+
+export default Greeting;
