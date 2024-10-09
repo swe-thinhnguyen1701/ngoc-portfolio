9e8c5a73eab43563cba59d64eea152b118801f64
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-10-09 17:25:52 -0500

    update education component

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,30 +1,40 @@
-function Education () {
+import marquette from "../assets/marquette-uni.webp";
+import davis from "../assets/davis-uni.webp";
+import deAnza from "../assets/deanza-college.webp";
+
+function Education() {
     const EDUCATION_DATA = [
         {
             school: 'Marquette University',
             degree: 'Direct Entry MSN',
-            date: '09/2023 - Present'
+            date: '09/2023 - Present',
+            image: marquette
         },
         {
             school: 'UC Davis',
             degree: 'B.S. Neurobiology, Physiology and Behavior',
-            date: '09/2019 - 06/2021'
+            date: '09/2019 - 06/2021',
+            image: davis
         },
         {
             school: 'De Anza College',
             degree: 'A.S. Biological Science',
-            date: '06/2016 - 06/2019'
+            date: '06/2016 - 06/2019',
+            image: deAnza
         }
     ]
     return (
-        <section className="education" id="education">
-            <div className="education-title">
+        <section className="education">
+            <div className="title">
                 <span>Education</span>
             </div>
-            <div className="education-container">
+            <ul className="list education-container">
                 {EDUCATION_DATA.map((education, index) => {
                     return (
-                        <div className="education-group" key={index}>
+                        <li className="education-group" key={index}>
+                            <div className={`icon-container icon-${index}`}>
+                                <img className="icon" src={education.image} alt="school logo" />
+                            </div>
                             <div className="program">
                                 <div className="school">
                                     {education.school}
@@ -32,14 +42,15 @@ function Education () {
                                 <div className="degree">
                                     {education.degree}
                                 </div>
+                                <div className="date">
+                                    {education.date}
+                                </div>
                             </div>
-                            <div className="date">
-                                {education.date}
-                            </div>
-                        </div>
+                        </li>
                     );
                 })}
-            </div>
+                <div className="time-line"></div>
+            </ul>
         </section>
     )
 }
