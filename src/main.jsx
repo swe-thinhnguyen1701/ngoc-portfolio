db60399717242c06071db093270d6573893a2c8e
Charles Nguyen <swe.thinhnguyen@gmail.com>
2024-10-09 17:27:48 -0500

    update links

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,36 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import App from './App.jsx'
 import './index.css'
 
+import HomePage from "./pages/HomePage.jsx";
+import AboutPage from "./pages/AboutPage.jsx";
+import ResumePage from "./pages/ResumePage.jsx";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                index: true,
+                element: <HomePage />,
+            },
+            {
+                path: "about",
+                element: <AboutPage />,
+            },
+            {
+                path: "resume",
+                element: <ResumePage />,
+            },
+        ],
+    },
+]);
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
